2f997fb41a0a74553b08210809d857c0a6f71d7f
chenmingyi <chen_mingyi@gowild.cn>
2019-09-06 18:46:56 +0800

    .

--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ import Vue from 'vue'
 // import App from './App'
 // import router from './router'
 import Layout from './components/layout'
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+// Vue.config.productionTip = false
+let router = new VueRouter({
+  mode:'history',
 
 
-Vue.config.productionTip = false
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { Layout },
   template: '<Layout/>'
 })
