61c899a97d06bc00d7ae162dbe34fd0a961ff354
chenmingyi <chen_mingyi@gowild.cn>
2019-09-06 18:23:57 +0800

    .

---
+++ src/components/layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="app-header">header</div>
+    <div class="app-content">content</div>
+    <div class="app-footer">footer</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg:'',
+                price:5,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
