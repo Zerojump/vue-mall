953b8b3f91b15b378fe5746b40303910e6a2ef86
zerojump <bycmy@qq.com>
2019-09-07 22:47:43 +0800

    slides

--- src/main.js
+++ src/main.js
@@ -6,13 +6,20 @@ import Vue from 'vue'
 import Layout from './components/layout'
 import VueRouter from "vue-router"
 import VueResource from 'vue-resource'
+import IndexPage from './pages/index'
+
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 // Vue.config.productionTip = false
 let router = new VueRouter({
   mode:'history',
-
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
 
 })
 
